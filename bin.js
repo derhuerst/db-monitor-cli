720034f73033026f2908e0c5d744fe65a322f689
Jannis R <mail@jannisr.de>
2017-05-20 01:49:37 +0200

    implementation :clap:

--- bin.js
+++ bin.js
@@ -1,4 +1,47 @@
 #!/usr/bin/env node
 'use strict'
 
-// todo
+const minimist = require('minimist')
+const monitor = require('db-monitor')
+const ndjson = require('ndjson')
+
+const argv = minimist(process.argv.slice(2))
+
+if (argv.help || argv.h) {
+	process.stdout.write(`
+Usage:
+    db-monitor [--interval milliseconds] station-id [...]
+
+Options:
+    --interval <ms>   How often to query for each station.
+
+Examples:
+    db-monitor 8011167
+    db-monitor --interval 5000 8011167 8002553
+\n`)
+	process.exit(0)
+}
+
+let interval = argv.interval || argv.i || null
+if (interval) {
+	interval = parseInt(interval)
+	if (Number.isNaN(interval)) {
+		process.stdout.write('--interval must be a number.')
+		process.exit(1)
+	}
+}
+
+const stations = argv._.map((id) => id + '')
+if (stations.length === 0) {
+	process.stdout.write('One or more station IDs must be given.')
+	process.exit(1)
+}
+
+const src = monitor(stations, interval)
+src
+.pipe(ndjson.stringify())
+.pipe(process.stdout)
+
+process.on('SIGINT', () => {
+	src.stop()
+})
